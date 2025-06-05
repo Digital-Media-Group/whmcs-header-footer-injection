f6d3489f60080f5928641693d0ae4a972acb57fc
Digital-Media-Group <119053220+Digital-Media-Group@users.noreply.github.com>
2025-06-05 12:20:37 +0200

    Update hooks.php

--- kn_header_footer_injection/hooks.php
+++ kn_header_footer_injection/hooks.php
@@ -1,44 +1,37 @@
 <?php
 use WHMCS\Database\Capsule;
 
-function arafat_hfi_getData()
+function kn_hfi_getData()
 {
-    if(isset($_POST["arafat_hfi"]))
-        return $_POST["arafat_hfi"];
-    
-    $query = Capsule::table('tbladdonmodules')->where('module', '=' , 'arafatkn_header_footer_injection');
-
+    if (isset($_POST["kn_hfi"])) {
+        return $_POST["kn_hfi"];
+    }
+    $query = Capsule::table('tbladdonmodules')->where('module', '=', 'kn_header_footer_injection');
     $data = [];
-    foreach($query->get() as $res)
+    foreach ($query->get() as $res)
         $data[$res->setting] = $res->value;
-
-    $_POST["arafat_hfi"] = $data;
-
-    return $_POST["arafat_hfi"];
+    $_POST["kn_hfi"] = $data;
+    return $_POST["kn_hfi"];
 }
 
-function arafat_hfi_head($vars)
-{   
-    $data = arafat_hfi_getData();
-    
-    return $data["head-enable"] == "on" && $data["head"]? trim($data["head"]): '';
-}
-
-function arafat_hfi_header($vars) 
-{   
-    $data = arafat_hfi_getData();
-    
-    return $data["header-enable"] == "on" && $data["header"]? trim($data["header"]): '';
-}
-
-function arafat_hfi_footer($vars) 
+function kn_hfi_head($vars)
 {
-	$data = arafat_hfi_getData();
-    
-    return $data["footer-enable"] == "on" && $data["footer"]? trim($data["footer"]): '';
+    $data = kn_hfi_getData();
+    return ($data["head-enable"] == "on" && $data["head"]) ? trim($data["head"]) : '';
 }
 
-add_hook("ClientAreaHeadOutput",1,"arafat_hfi_head");
-add_hook("ClientAreaHeaderOutput",1,"arafat_hfi_header");
-add_hook("ClientAreaFooterOutput",1,"arafat_hfi_footer");
+function kn_hfi_header($vars)
+{
+    $data = kn_hfi_getData();
+    return ($data["header-enable"] == "on" && $data["header"]) ? trim($data["header"]) : '';
+}
 
+function kn_hfi_footer($vars)
+{
+    $data = kn_hfi_getData();
+    return ($data["footer-enable"] == "on" && $data["footer"]) ? trim($data["footer"]) : '';
+}
+
+add_hook("ClientAreaHeadOutput", 1, "kn_hfi_head");
+add_hook("ClientAreaHeaderOutput", 1, "kn_hfi_header");
+add_hook("ClientAreaFooterOutput", 1, "kn_hfi_footer");
